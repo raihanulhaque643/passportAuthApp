5267f5a6a17081f212a72b0ee1e5a3885b46a766
Raihanul Haque <raihan.hd.500@gmail.com>
2021-02-28 23:43:40 +0600

    login setup done with json responses

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 router.post(
     '/signup',
     passport.authenticate('signup', { session: false, failWithError: true }),
     async (req, res, next) => {
-        res.json({
-          message: 'Signup successful',
-          user: req.user
-        });
+        if (req.user) {
+          res.json({
+            message: 'Signup successful',
+            user: req.user
+          });
+        } else {
+          next()
+        }
     },
     async (error, req, res, next) => {
         res.json({
@@ -19,5 +24,44 @@ router.post(
         })
     }
   );
+
+router.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error(info.message);
+
+            return next(error);
+          }
+
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
+
+              return res.json({ token });
+            }
+          );
+        } catch (error) {
+          return next();
+        }
+      }
+    )(req, res, next);
+  },
+  async (error, req, res, next) => {
+    res.json({
+      message: 'Login failed',
+      error: error.message
+    })
+  }
+);
 
 module.exports = router
